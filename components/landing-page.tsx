27807752fb79dab934fe622fdb5a1a56774ca54d
0x6f6b <jackmertensdev@gmail.com>
2024-05-29 15:49:35 +0100

    more appropriate icon

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import { InteractiveMap } from "./interactive-map";
 import Image from "next/image";
+import { Map } from "lucide-react";
 
 export function LandingPage() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
-          <LandmarkIcon className="h-6 w-6" />
+          <Map className="h-6 w-6" />
           <span className="sr-only">Landmine Map</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
