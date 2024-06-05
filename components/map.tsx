e8274c90bab6d16e6bd867b8e2ab79eef2bdce68
0x6f6b <jackmertensdev@gmail.com>
2024-06-05 14:54:08 +0100

    updated MAP

--- components/map.tsx
+++ components/map.tsx
@@ -1,75 +1,85 @@
 "use client";
 import GoogleMapReact from "google-map-react";
+import GoogleMap from "google-maps-react-markers";
+
 import utmObj from "utm-latlng";
 
 import { features } from "../dataset.json";
+import { RefAttributes, useRef, useState } from "react";
+import { LucideProps, MapPin } from "lucide-react";
 
 const utm = new utmObj();
 
-interface AnyReactComponentProps {
-  key: number;
+interface MapPinProps
+  extends Omit<LucideProps, "ref">,
+    RefAttributes<SVGSVGElement> {
   lat: number;
   lng: number;
-  text: string | null;
+  markerId?: string;
+  coordinates: [number, number];
 }
 
-const AnyReactComponent = ({ text }: AnyReactComponentProps) => (
-  <div
-    style={{
-      backgroundColor: "red",
-      color: "white",
-      height: 25,
-      width: 25,
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: 50,
-    }}
-  >
-    {text}
-  </div>
-);
+const Marker: React.FC<MapPinProps> = ({
+  lat,
+  lng,
+  markerId,
+  coordinates,
+  ...props
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const mineLocations = utm.convertUtmToLatLng(
+    coordinates[0],
+    coordinates[1],
+    48,
+    "N"
+  ) as { lat: number; lng: number };
+
+  return (
+    <MapPin
+      markerId={markerId as string}
+      lat={lat}
+      lng={lng}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+      {...props}
+    />
+  );
+};
 
 const Map = () => {
+  const onMarkerClick = (
+    e: any,
+    {
+      markerId,
+      lat,
+      lng,
+    }: {
+      markerId: any;
+      lat: number;
+      lng: number;
+    }
+  ) => {
+    console.log("This is ->", markerId);
+  };
+
   return (
-    <div style={{ height: "100vh", width: "90%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{
-          key: process.env.NEXT_PUBLIC_GOOGLE_MAPS as string,
-        }}
-        defaultCenter={{
-          lat: 12.5657,
-          lng: 104.991,
-        }}
+    <div style={{ height: "80vh", width: "90%" }}>
+      <GoogleMap
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS as string}
+        defaultCenter={{ lat: 12.5657, lng: 104.991 }}
         defaultZoom={7}
+        mapMinHeight="80vh"
+        // onGoogleApiLoaded={onGoogleApiLoaded}
+        onChange={(map) => console.log("Map moved", map)}
       >
         {features.map((feature) => (
-          <AnyReactComponent
+          <MapPin
             key={feature.properties.IncidentID}
-            lat={
-              (
-                utm.convertUtmToLatLng(
-                  feature.geometry.coordinates[0],
-                  feature.geometry.coordinates[1],
-                  48,
-                  "N"
-                ) as { lat: number; lng: number }
-              ).lat
-            }
-            lng={
-              (
-                utm.convertUtmToLatLng(
-                  feature.geometry.coordinates[0],
-                  feature.geometry.coordinates[1],
-                  48,
-                  "N"
-                ) as { lat: number; lng: number }
-              ).lng
-            }
-            text={feature.properties.MINE_TYPE}
+            coordinates={feature.geometry.coordinates}
+            markerId={feature.properties.IncidentID}
           />
         ))}
-      </GoogleMapReact>
+      </GoogleMap>
     </div>
   );
 };
